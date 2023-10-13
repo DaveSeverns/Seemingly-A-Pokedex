6688f37c90d979dc660e8e5d3d51162dee229846
Dave Severns <david.w.severns@gmail.com>
2023-10-12 22:59:36 -0400

    add landscape specific layout of poke list card

--- app/src/main/java/com/sevdotdev/seeminglyapokedex/ui/pokelist/PokeListItemCard.kt
+++ app/src/main/java/com/sevdotdev/seeminglyapokedex/ui/pokelist/PokeListItemCard.kt
@@ -1,5 +1,6 @@
 package com.sevdotdev.seeminglyapokedex.ui.pokelist
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -16,9 +18,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -39,6 +43,7 @@ internal fun PokeListItemCard(
     pokemon: PokemonListItem,
     onPokemonClicked: (name: String) -> Unit,
 ) {
+    val orientation = LocalConfiguration.current.orientation
     Card(
         elevation = CardDefaults.cardElevation(
             defaultElevation = 4.dp
@@ -46,40 +51,68 @@ internal fun PokeListItemCard(
         modifier = Modifier
             .fillMaxWidth(),
         onClick = { onPokemonClicked(pokemon.name) }) {
-        val painter = rememberAsyncImagePainter(
-            model = ImageRequest.Builder(LocalContext.current).data(pokemon.imageUrl)
-                .crossfade(true).placeholder(R.drawable.pokeball_ic).build()
-        )
-        Column(Modifier.fillMaxWidth()) {
-            Image(
-                painter = painter, contentDescription = null,
-                Modifier
-                    .aspectRatio(1.71f)
-                    .background(MaterialTheme.colorScheme.secondary),
 
-                )
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .padding(vertical = 8.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = pokemon.id, style = MaterialTheme.typography.titleLarge)
-                    Row {
-                        TypePill(
-                            typeName = stringResource(id = pokemon.type.toStringResource()),
-                            typeColor = pokemon.type.toColor()
-                        )
-                    }
+        when (orientation) {
+
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                Row(Modifier.fillMaxWidth()) {
+                    CardContent(
+                        pokemon = pokemon,
+                        1f,
+                        120.dp
+                    )
                 }
-                Text(
-                    text = pokemon.name.replaceFirstChar { c -> c.uppercase() },
-                    style = MaterialTheme.typography.headlineLarge
+            }
+
+            else -> {
+                Column(Modifier.fillMaxWidth()) {
+                    CardContent(pokemon = pokemon)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CardContent(
+    pokemon: PokemonListItem,
+    imageAspectRatio: Float = 1.71f,
+    imageMaxHeight: Dp = Dp.Infinity
+) {
+
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current).data(pokemon.imageUrl)
+            .crossfade(true).placeholder(R.drawable.pokeball_ic).build()
+    )
+    Image(
+        painter = painter, contentDescription = null,
+        Modifier
+            .height(imageMaxHeight)
+            .aspectRatio(imageAspectRatio)
+            .background(MaterialTheme.colorScheme.secondary),
+
+        )
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .padding(vertical = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = pokemon.id, style = MaterialTheme.typography.titleLarge)
+            Row {
+                TypePill(
+                    typeName = stringResource(id = pokemon.type.toStringResource()),
+                    typeColor = pokemon.type.toColor()
                 )
             }
         }
+        Text(
+            text = pokemon.name.replaceFirstChar { c -> c.uppercase() },
+            style = MaterialTheme.typography.headlineLarge
+        )
     }
 }
 
